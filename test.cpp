f1c8638201c8ff13490b2340952517e7e1225863
mini <gusals121234@gmail.com>
2022-03-29 15:45:49 +0900

    stduy strlen function

--- test.cpp
+++ test.cpp
@@ -3,25 +3,18 @@
 
 int main(void)
 {
-   char str1[80] = "stawberry";// char 배열에 문자열 초기화
-   char str2[80] = "apple";// char 배열에 문자열 초기화
+   char str1[80], str2[80];   // declare arrays to put strings
+   char * resp;   // declare pointer to select array that have longer lengths
+   // 여기서 포인터 resp는 마치 새로운 배열을 선언한것처럼 사용된다. 
 
-   char * ps1 = "banana";  // 포인터로 문자열 상수 연결.
-   char * ps2 = str2;  // 포인터로 배열 연결.
-
-   printf(" 최초 문자열 : %s\n", str1);
-   strcpy(str1, str2);        // 다른 char 배열의 문자열 복사.
-   printf(" 바뀐 문자열 : %s\n", str1);
-
-   strcpy(str1, ps1);        // 문자열 상수를 연결한 포인터 사용
-   printf(" 바뀐 문자열 : %s\n", str1);
-
-   strcpy(str1, ps2);        // 배열을 연결한 포인터 사용
-   printf(" 바뀐 문자열 : %s\n", str1);
-
-   strcpy(str1, "banana");        // 문자열 상수 사용
-   printf(" 바뀐 문자열 : %s\n", str1);
-
-
+   printf (" 2개의 과일 이름 입력 : ");
+   scanf("%s%s", str1, str2);
+   // ex) sizeof(str1) =80 , strlen(str1) = length of input string
+   
+   if(strlen(str1)>strlen(str2))
+   resp = str1;
+   else
+   resp = str2;
+   printf("이름이 긴 과일은 :  %s\n", resp);
    return 0;
 }
